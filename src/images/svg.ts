b9987e4a816ec90985607d23be9c28f948121f50
Pier Fumagalli <pier@usrz.com>
2024-05-02 13:31:42 +0200

    Optimize path generation by using a single array

--- src/images/svg.ts
+++ src/images/svg.ts
@@ -8,28 +8,25 @@ export function generateSvg(code: QrCode, options?: QrCodeImageOptions): string 
   const size = code.size + 2 * margin
   const scaled = size * scale
 
-  const chunks: string[] = [
+  const chunks: (string | number)[] = [
     `<svg xmlns="http://www.w3.org/2000/svg" width="${scaled}" height="${scaled}" viewBox="0 0 ${size} ${size}">`,
     '<path d="', // beginning of the path "d" attribute...
   ]
 
   // Push all the SVG path components...
-  chunks.push(...generatePaths(code).map((path) => {
-    const array: (string | number)[] = []
-
+  generatePaths(code).forEach((path) => {
     for (let k = 0; k < path.length; k++) {
       const item = path[k]!
       switch (item[0]) {
         case 'M': // move
-          array.push(`M${item[1] + margin} ${item[2] + margin}`)
+          chunks.push(`M${item[1] + margin} ${item[2] + margin}`)
           break
         default: // draw path
-          array.push(...item)
+          chunks.push(...item)
       }
     }
-    array.push('z') // done
-    return array.join('')
-  }))
+    chunks.push('z') // done
+  })
 
   chunks.push('"/></svg>') // close the path "d" attribute
   return chunks.join('')
