6c8cc81a26ace49036059cb4777435522a622276
Pier Fumagalli <pier@usrz.com>
2024-05-02 17:17:52 +0200

    Always compare the *enflated* values (it seems NodeJS `deflate` varies depending on version)

--- test/12-pdf.test.ts
+++ test/12-pdf.test.ts
@@ -1,42 +1,83 @@
-import fs from 'node:fs/promises'
+import { writeFile } from 'node:fs/promises'
+import { inflate } from 'node:zlib'
 
 import { generate, generatePdf, generateQrCode } from '../src/index'
 
 describe('QR Code as a PDF', () => {
+  let data: Uint8Array | undefined = undefined
+
   it('should generate a PDF QR code for a simple message', async () => {
     const code = generateQrCode('foo')
     const pdf = await generatePdf(code)
 
-    expect(new TextDecoder().decode(pdf.slice(0, 277))).toEqual([
-      '%PDF-1.0\n', // newline after header
+    const preamble = new TextDecoder().decode(pdf.slice(0, 277)).split('\n')
+    expect(preamble).toEqual([
+      '%PDF-1.0', // newline after header
+      '', // newline after header
       '1 0 obj << /Type /Catalog /Pages 2 0 R >> endobj',
       '2 0 obj << /Type /Pages /Count 1 /Kids [ 3 0 R ] >> endobj',
       '3 0 obj << /Type /Page /Parent 2 0 R /Resources <<>> /Contents 4 0 R /MediaBox [ 0 0 261 261 ] >> endobj',
-      '4 0 obj << /Length 587 /Filter /FlateDecode >> stream',
+      expect.toMatch(/^4 0 obj << \/Length \d\d\d \/Filter \/FlateDecode >> stream$/),
       '', // rest is the deflated content after a newline...
+    ])
+
+    const length = parseInt(preamble[5]!.substring(19, 22))
+
+    const inflated = await new Promise<Buffer>((resolve, reject) => {
+      const buffer = Buffer.from(pdf.slice(277, 277 + length))
+      inflate(buffer, (error, result) => {
+        if (error) return reject(error)
+        return resolve(result)
+      })
+    })
+
+    expect(inflated.toString('utf-8')).toEqual([
+      '9 0 0 9 0 0 cm',
+      '4 25 m 11 25 l 11 18 l 4 18 l h',
+      '15 25 m 17 25 l 17 24 l 16 24 l 16 23 l 15 23 l h',
+      '18 25 m 25 25 l 25 18 l 18 18 l h',
+      '5 24 m 5 19 l 10 19 l 10 24 l h',
+      '13 24 m 14 24 l 14 22 l 13 22 l 13 20 l 14 20 l 14 19 l 13 19 l 13 18 l 14 18 l 14 19 l 15 19 l 15 17 l 16 17 l 16 16 l 18 16 l 18 17 l 23 17 l 23 16 l 22 16 l 22 14 l 23 14 l 23 13 l 21 13 l 21 16 l 19 16 l 19 15 l 20 15 l 20 14 l 19 14 l 19 13 l 17 13 l 17 11 l 16 11 l 16 13 l 14 13 l 14 12 l 13 12 l 13 13 l 11 13 l 11 14 l 12 14 l 12 15 l 15 15 l 15 14 l 16 14 l 16 15 l 15 15 l 15 16 l 14 16 l 14 17 l 12 17 l 12 23 l 13 23 l h',
+      '19 24 m 19 19 l 24 19 l 24 24 l h',
+      '6 23 m 9 23 l 9 20 l 6 20 l h',
+      '16 23 m 17 23 l 17 20 l 16 20 l h',
+      '20 23 m 23 23 l 23 20 l 20 20 l h',
+      '15 20 m 16 20 l 16 19 l 15 19 l h',
+      '16 19 m 17 19 l 17 18 l 16 18 l h',
+      '4 17 m 5 17 l 5 16 l 6 16 l 6 17 l 11 17 l 11 16 l 8 16 l 8 15 l 9 15 l 9 14 l 6 14 l 6 13 l 5 13 l 5 14 l 4 14 l h',
+      '11 16 m 12 16 l 12 15 l 11 15 l h',
+      '24 16 m 25 16 l 25 15 l 24 15 l h',
+      '10 15 m 11 15 l 11 14 l 10 14 l h',
+      '17 15 m 17 14 l 18 14 l 18 15 l h',
+      '23 15 m 24 15 l 24 14 l 23 14 l h',
+      '24 14 m 25 14 l 25 13 l 24 13 l h',
+      '4 13 m 5 13 l 5 12 l 4 12 l h',
+      '6 13 m 8 13 l 8 12 l 6 12 l h',
+      '9 13 m 11 13 l 11 12 l 9 12 l h',
+      '19 13 m 20 13 l 20 11 l 19 11 l h',
+      '23 13 m 24 13 l 24 12 l 23 12 l h',
+      '12 12 m 13 12 l 13 11 l 12 11 l h',
+      '14 12 m 15 12 l 15 11 l 14 11 l h',
+      '21 12 m 22 12 l 22 10 l 21 10 l 21 8 l 20 8 l 20 9 l 19 9 l 19 10 l 20 10 l 20 11 l 21 11 l h',
+      '24 12 m 25 12 l 25 11 l 24 11 l h',
+      '4 11 m 11 11 l 11 4 l 4 4 l h',
+      '15 11 m 16 11 l 16 10 l 15 10 l h',
+      '17 11 m 18 11 l 18 10 l 17 10 l h',
+      '23 11 m 24 11 l 24 10 l 25 10 l 25 9 l 23 9 l 23 8 l 22 8 l 22 10 l 23 10 l h',
+      '5 10 m 5 5 l 10 5 l 10 10 l h',
+      '12 10 m 14 10 l 14 8 l 13 8 l 13 6 l 12 6 l h',
+      '6 9 m 9 9 l 9 6 l 6 6 l h',
+      '15 9 m 16 9 l 16 8 l 17 8 l 17 9 l 18 9 l 18 8 l 19 8 l 19 7 l 20 7 l 20 6 l 19 6 l 19 5 l 17 5 l 17 4 l 16 4 l 16 5 l 15 5 l 15 4 l 12 4 l 12 5 l 13 5 l 13 6 l 16 6 l 16 7 l 15 7 l h',
+      '21 8 m 22 8 l 22 7 l 21 7 l h',
+      '17 7 m 17 6 l 18 6 l 18 7 l h',
+      '21 6 m 22 6 l 22 5 l 21 5 l h',
+      '19 5 m 20 5 l 20 4 l 19 4 l h',
+      '22 5 m 24 5 l 24 4 l 22 4 l h',
+      'f',
+      '', // final newline
     ].join('\n'))
 
-    expect(Buffer.from(pdf.slice(277 -1, 277 + 587 + 1)).toString('hex')).toEqual([ // include newlines
-      '0a789c5d955dcedc300845dfbf55640931fe5f4fa5aa0fcdfe5f6b730fce4c3552cec80', // newline before deflated data
-      '66ce092cceb5ebfe9cf5fcf4fb9ac5ecf95d2e6dfcd34168bf0e727550c3a068b65b3bd',
-      'cc9b555c1e431e56e5b11e1e6b3d8859b7eb73adf5b9d7ef438fb8226419a4c2198bb69',
-      '95fdeac4345c82f07ebe373bfbeecbafb61e386c1bdbe123adcebebf0c3c27a70e76ee9',
-      'a5c7992fbd12f7cbc27a30abb687897b05337904a9c361a679418f6b2f2b7907e9dde1f',
-      'ffb8d73829d38503dcfa7e7938e4d55d8ca213d75993c4b77ee31d5b8262c7fb6b7baa8',
-      '803a7b2c16dcc238d3d0c05ebf8f52ef1da31ddfafaefb29eb9f9fa29d8e3e5a28d3137',
-      '565ee3c29433be81437b897c741f5c402452e202b1c289a30d4ee811eaf6efbec561257',
-      'ee4516c6858c3eed75592457d6737c8e02ee734ac7a2b3335e724a964544b5f2ad72dda',
-      '3708fc23d329639ea97553f3235656a68c077877687965bec4eed7e8ad8544a0b8db9c1',
-      '9e9ecc1425a6279d0c3219c4bd8c0c8861fbdff33539899a2b8666eb71d518ea49cc00a',
-      '724599811dd769935f570e87a60ea9220a1dac3846f3a35366a6cd438914b8a1a27ea94',
-      '5427c9a9c41468fbe3dd7193c77d94e016038b81450f0bcbb28833376fee02a7ca0a86c',
-      'a303eab91239a3b6d4d540912c46d4cdb893316871a0318898682a6bf44a60b4353d922',
-      'efa9b4a7b21e4a094c250a865a01ba1a017861033e79809725e0550978d182aacbd78f1',
-      'c5aa0cba1879a86c444f5bae4d0a3575d43cb77091cdf265fbe4a55be35e6a56a5cf8e6',
-      'f0a96196ed62d899f5a210dafdfdf30f9541a1c40a', // newline after deflated data
-    ].join(''))
-
-    expect(new TextDecoder().decode(pdf.slice(277 + 587))).toEqual([
+    expect(new TextDecoder().decode(pdf.slice(277 + length))).toEqual([
       '', // the deflated content ends and a newline is added
       'endstream',
       'endobj',
@@ -49,7 +90,7 @@ describe('QR Code as a PDF', () => {
       '0000000223 00000 n ',
       'trailer << /Root 1 0 R /Size 5 >>',
       'startxref',
-      '882',
+      `${length + 295}`,
       '%%EOF',
       '', // there's always a newline at the end of the file
     ].join('\n'))
@@ -58,37 +99,75 @@ describe('QR Code as a PDF', () => {
   it('should generate a PDF QR code', async () => {
     const pdf = await generate('https://www.juit.com/', 'pdf', { ecLevel: 'L', url: true, scale: 3, margin: 1 })
 
-    await fs.writeFile('./test.pdf', pdf)
+    await writeFile('./test.pdf', pdf)
 
-    expect(new TextDecoder().decode(pdf.slice(0, 275))).toEqual([
-      '%PDF-1.0\n', // newline after header
+    const preamble = new TextDecoder().decode(pdf.slice(0, 275)).split('\n')
+    expect(preamble).toEqual([
+      '%PDF-1.0', // newline after header
+      '', // newline after header
       '1 0 obj << /Type /Catalog /Pages 2 0 R >> endobj',
       '2 0 obj << /Type /Pages /Count 1 /Kids [ 3 0 R ] >> endobj',
       '3 0 obj << /Type /Page /Parent 2 0 R /Resources <<>> /Contents 4 0 R /MediaBox [ 0 0 69 69 ] >> endobj',
-      '4 0 obj << /Length 539 /Filter /FlateDecode >> stream',
+      expect.toMatch(/^4 0 obj << \/Length \d\d\d \/Filter \/FlateDecode >> stream$/),
       '', // rest is the deflated content after a newline...
+    ])
+
+    const length = parseInt(preamble[5]!.substring(19, 22))
+
+    const inflated = await new Promise<Buffer>((resolve, reject) => {
+      const buffer = Buffer.from(pdf.slice(275, 275 + length))
+      inflate(buffer, (error, result) => {
+        if (error) return reject(error)
+        return resolve(result)
+      })
+    })
+
+    expect(inflated.toString('utf-8')).toEqual([
+      '3 0 0 3 0 0 cm',
+      '1 22 m 8 22 l 8 15 l 1 15 l h',
+      '12 22 m 14 22 l 14 19 l 13 19 l 13 21 l 12 21 l h',
+      '15 22 m 22 22 l 22 15 l 15 15 l h',
+      '2 21 m 2 16 l 7 16 l 7 21 l h',
+      '11 21 m 12 21 l 12 18 l 11 18 l h',
+      '16 21 m 16 16 l 21 16 l 21 21 l h',
+      '3 20 m 6 20 l 6 17 l 3 17 l h',
+      '17 20 m 20 20 l 20 17 l 17 17 l h',
+      '9 19 m 10 19 l 10 18 l 11 18 l 11 16 l 10 16 l 10 17 l 9 17 l h',
+      '12 18 m 14 18 l 14 14 l 13 14 l 13 13 l 14 13 l 14 12 l 12 12 l 12 15 l 13 15 l 13 16 l 12 16 l h',
+      '9 16 m 10 16 l 10 15 l 9 15 l h',
+      '11 16 m 12 16 l 12 15 l 11 15 l h',
+      '1 14 m 3 14 l 3 13 l 2 13 l 2 12 l 1 12 l h',
+      '6 14 m 9 14 l 9 13 l 7 13 l 7 12 l 4 12 l 4 13 l 6 13 l h',
+      '17 14 m 19 14 l 19 13 l 17 13 l h',
+      '10 13 m 11 13 l 11 12 l 12 12 l 12 10 l 10 10 l h',
+      '16 13 m 17 13 l 17 12 l 18 12 l 18 11 l 16 11 l h',
+      '7 12 m 9 12 l 9 10 l 8 10 l 8 11 l 7 11 l h',
+      '14 12 m 15 12 l 15 10 l 18 10 l 18 11 l 21 11 l 21 10 l 22 10 l 22 7 l 21 7 l 21 8 l 20 8 l 20 4 l 19 4 l 19 3 l 18 3 l 18 5 l 19 5 l 19 6 l 16 6 l 16 7 l 17 7 l 17 8 l 18 8 l 18 9 l 16 9 l 16 8 l 15 8 l 15 4 l 14 4 l 14 5 l 13 5 l 13 4 l 12 4 l 12 3 l 10 3 l 10 4 l 11 4 l 11 6 l 14 6 l 14 10 l 13 10 l 13 11 l 14 11 l h',
+      '21 12 m 22 12 l 22 11 l 21 11 l h',
+      '5 11 m 6 11 l 6 10 l 5 10 l h',
+      '1 10 m 5 10 l 5 9 l 1 9 l h',
+      '6 10 m 8 10 l 8 9 l 6 9 l h',
+      '9 10 m 10 10 l 10 9 l 11 9 l 11 7 l 10 7 l 10 6 l 9 6 l h',
+      '12 10 m 13 10 l 13 9 l 12 9 l h',
+      '19 10 m 19 9 l 20 9 l 20 10 l h',
+      '1 8 m 8 8 l 8 1 l 1 1 l h',
+      '12 8 m 13 8 l 13 7 l 12 7 l h',
+      '2 7 m 2 2 l 7 2 l 7 7 l h',
+      '3 6 m 6 6 l 6 3 l 3 3 l h',
+      '21 6 m 22 6 l 22 5 l 21 5 l h',
+      '16 5 m 17 5 l 17 4 l 16 4 l h',
+      '15 4 m 16 4 l 16 3 l 17 3 l 17 2 l 16 2 l 16 1 l 15 1 l h',
+      '20 4 m 22 4 l 22 2 l 21 2 l 21 3 l 20 3 l h',
+      '9 3 m 10 3 l 10 1 l 9 1 l h',
+      '12 3 m 13 3 l 13 2 l 12 2 l h',
+      '19 3 m 20 3 l 20 2 l 19 2 l h',
+      '13 2 m 14 2 l 14 1 l 13 1 l h',
+      '17 2 m 19 2 l 19 1 l 17 1 l h',
+      'f',
+      '', // final newline
     ].join('\n'))
 
-    expect(Buffer.from(pdf.slice(275 - 1, 275 + 539 + 1)).toString('hex')).toEqual([ // include newlines
-      '0a789c5d955172ee200885dfb38a2c41408daea73377fe8766ffaf05ce2169efb4e3972', // newline before deflated data
-      '8103c82bf9dcdff2cc7affb9053f5bccf15f876c87008f03944b12c1deb4ed9417ba812',
-      '54d03d063c54e1e103428e8a99a66e70caf4f7ab50fe82e58ae8941514d02d262d265cf',
-      'da5c8189e54738319f876c8e530c0fd2f2cfb90eb3ee48a0fb4d8b139ff40e326dbdf14',
-      '841f8cf96238eee70b9973aa06871eff50ad689c2f2af75a1cb42b4ececfca7032c3ca6',
-      '02083512ac2405fcfc1d469115fbd4f66c484f481a20a346d276c376c378cae0761d41f',
-      '1824b7d23a5d85be4267b91e8b164f776666ccf07f2d1ab7d8eafce171bdb1d272bdccc',
-      'a99e0e74883cc5f917f43a913826db0fe3a8ca35e9575db18b5fd8a1e55576cac73f2c2',
-      '34b150620445200c0189814962620b040b94587020364c8885a4898ef222584c4487bc8',
-      '44164a2e328880977023ad84be13c14548182ca130cfe56ec738c7888feccf78940e339',
-      'df78b8eb7d605f396625b6bcad78763bfd77f544634fb06036b2272e4c12332b613efd9',
-      'aaeef9e36444164a9d03b27b4159e8c5726b5504f689d8abc107821ee85b8b827827111',
-      '6a162046acd83953a099fbb3ec502b7527c49dd07640da51bd31d01a0395d25114fde4d',
-      'ddc7171729aed4328260961f1e3ab2d3d15d5a248392e5cc02087f11c0cc7c05212f45c',
-      'e961d0c3a0075b5c4b67c3c5cc808a56e06a58e3a788f5c6f2ab9b46714474125e0db9f',
-      'aeff8011e6964a30a', // newline after deflated data
-    ].join(''))
-
-    expect(new TextDecoder().decode(pdf.slice(275 + 539))).toEqual([
+    expect(new TextDecoder().decode(pdf.slice(275 + length))).toEqual([
       '', // the deflated content ends and a newline is added
       'endstream',
       'endobj',
@@ -101,36 +180,21 @@ describe('QR Code as a PDF', () => {
       '0000000221 00000 n ',
       'trailer << /Root 1 0 R /Size 5 >>',
       'startxref',
-      '832',
+      `${length + 293}`,
       '%%EOF',
       '', // there's always a newline at the end of the file
     ].join('\n'))
+
+    // used for the data URL test
+    data = pdf
   })
 
   it('should generate a PDF QR code as a data URL', async () => {
-    const pdf = await generate('https://www.juit.com/', 'pdfData', { ecLevel: 'L', url: true, scale: 3, margin: 1 })
+    if (! data) return skip()
 
-    expect(pdf).toEqual([
-      'data:application/pdf;base64,JVBERi0xLjAKCjEgMCBvYmogPDwgL1R5cGUgL0NhdGF',
-      'sb2cgL1BhZ2VzIDIgMCBSID4+IGVuZG9iagoyIDAgb2JqIDw8IC9UeXBlIC9QYWdlcyAvQ2',
-      '91bnQgMSAvS2lkcyBbIDMgMCBSIF0gPj4gZW5kb2JqCjMgMCBvYmogPDwgL1R5cGUgL1BhZ',
-      '2UgL1BhcmVudCAyIDAgUiAvUmVzb3VyY2VzIDw8Pj4gL0NvbnRlbnRzIDQgMCBSIC9NZWRp',
-      'YUJveCBbIDAgMCA2OSA2OSBdID4+IGVuZG9iago0IDAgb2JqIDw8IC9MZW5ndGggNTM5IC9',
-      'GaWx0ZXIgL0ZsYXRlRGVjb2RlID4+IHN0cmVhbQp4nF2VUXLuIAiF37OKLEFAja6nM3f+h2',
-      'b/rwXOIWnvtOOXKBA8gr+dzf8sx6/7kFP1vM8V+HbIcAjwOUSxLB3rTtlBe6gSVNA9BjxU4',
-      'eEDQo6KmaZucMr096tQ/oLliuiUFRTQLSYtJlz9pcgYnlRzgxn4dsjlMMD9Lyz7kOs+5IoP',
-      'tNixOf9A4ybb3xSEH4z5Yjju5wuZc6oGhx7/UK1onC8q91octCtOzs/KcDLDymAgg1EqwkB',
-      'fz8HUaRFfvU9mxIT0gaIKNG0nbDdsN4yuB2HUHxgkt9I6XYW+Qme5HosWT3dmZszwfy0at9',
-      'jq/OFxvbHScr3Mypng50iDzF+Rf0OpE4JtsP46jKNelXXbGLX9ih5VV2ysc/LCNLFQYgRFI',
-      'AwBiYFJYmILBAuUWHAgNkyIhaSJjvIiWExEh7yEQWSi4yiICXcCOthL4TwUVIGCyhMM/lbs',
-      'c4x4iP7M94lA4znfeLjrfWBfOWYltryteHY7/Xf1RGNPsGA2sicuTBIzK2E+/Zqu7542REF',
-      'kqdA7J7QVnoxXJrVQT2idirwQeCHuhbi4J4JxEWoWIEas2DlToJn7s+xQK3UnxJ3QdkDaUb',
-      '0x0BoDldJRFP3k3dxxcXKa7UMoJglh8eOrLT0V1aJIOS5cwCCH8RwMx8BSEvRc6WHQw6AHW',
-      '1xLZ8PFzICKVuBqWOOniPXG8qubRnFEdBJeDbn67/gBHmlkowplbmRzdHJlYW0KZW5kb2Jq',
-      'CnhyZWYKMCA1CjAwMDAwMDAwMDAgNjU1MzUgZiAKMDAwMDAwMDAxMCAwMDAwMCBuIAowMDA',
-      'wMDAwMDU5IDAwMDAwIG4gCjAwMDAwMDAxMTggMDAwMDAgbiAKMDAwMDAwMDIyMSAwMDAwMC',
-      'BuIAp0cmFpbGVyIDw8IC9Sb290IDEgMCBSIC9TaXplIDUgPj4Kc3RhcnR4cmVmCjgzMgolJ',
-      'UVPRgo=',
-    ].join(''))
+    const pdf = await generate('https://www.juit.com/', 'pdfData', { ecLevel: 'L', url: true, scale: 3, margin: 1 })
+    expect(pdf).toMatch(/^data:application\/pdf;base64,/)
+
+    expect(new Uint8Array(Buffer.from(pdf.substring(28), 'base64'))).toEqual(data)
   })
 })
